da359438159478e943d39d94cfd5b883bf5aa4e8
Sebastian_Hernandez_Patio <sebastianhernandezpatio.2004@gmail.com>
2024-05-22 22:35:04 -0500

    Change use of the store, add useStore

--- src/app/container/book-list.container.tsx
+++ src/app/container/book-list.container.tsx
@@ -1,11 +1,12 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '../core/store/store';
+import { useSelector } from 'react-redux';
+import { RootState } from '../core/store/store';
 import { useEffect } from 'react';
 import { getCopies } from '../core/store/thunks/copies.thunk';
 import BooksList from '../ui/components/BookList';
+import { useAppDispatch } from '../core/hooks/useStore';
 
 const BooksListContainer: React.FC = () => {
-  const dispatch = useDispatch<AppDispatch>();
+  const dispatch = useAppDispatch();
   const books = useSelector((state: RootState) => state.copies.books);
   const status = useSelector((state: RootState) => state.copies.status);
   const error = useSelector((state: RootState) => state.copies.error);
@@ -17,11 +18,11 @@ const BooksListContainer: React.FC = () => {
   }, [status, dispatch]);
 
   return (
-    <div>
+    <>
       {status === 'loading' && <p>Loading...</p>}
       {status === 'failed' && <p>Error: {error}</p>}
       {status === 'succeeded' && <BooksList books={books} />}
-    </div>
+    </>
   );
 };
 
