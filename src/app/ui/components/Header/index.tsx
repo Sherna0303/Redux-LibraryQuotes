018d7d1f6e27c665e5b4a52e707b8f53937c6c56
Sebastian_Hernandez_Patio <sebastianhernandezpatio.2004@gmail.com>
2024-05-24 03:54:40 -0500

    Add cart button functionality in the header, add close aside icon

--- src/app/ui/components/Header/index.tsx
+++ src/app/ui/components/Header/index.tsx
@@ -2,8 +2,12 @@ import { ReactElement } from 'react';
 import './style.css';
 import { Paragraph } from '../../elements/Paragraph';
 import Icon from '../../elements/Icon/inde';
+import { useAside } from '../../../core/hooks/useAside';
+import { toggleAside } from '../../../core/store/reducers/aside.reducer';
 
 export const Header = (): ReactElement => {
+  const { dispatch, showAside } = useAside();
+
   return (
     <header className='header__container'>
       <div className="header__main">
@@ -11,7 +15,9 @@ export const Header = (): ReactElement => {
           <Icon color='white' size={32} icon='book-2'/>
           <Paragraph className='logo__text' text='BookStore'/>
         </div>
-        <Icon color='white' size={32} icon='shopping-cart'/>
+        <button className='header__cart' onClick={() => dispatch(toggleAside())}>
+          <Icon color='white' size={32} icon={showAside ? 'shopping-cart-x' : 'shopping-cart'}/>
+        </button>
       </div>
     </header>
   );
