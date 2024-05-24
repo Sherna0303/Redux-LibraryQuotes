68afcec0f92b4892027df6ea4becfa8c37ae39f9
Sebastian_Hernandez_Patio <sebastianhernandezpatio.2004@gmail.com>
2024-05-24 06:08:40 -0500

    Add styles to the carts section on the side

--- src/app/ui/components/Carts/index.tsx
+++ src/app/ui/components/Carts/index.tsx
@@ -3,6 +3,7 @@ import { Button } from '../../elements/Button';
 import { CartInfo } from '../../../core/models/cart-info.model';
 import { Title } from '../../elements/Title';
 import './style.css';
+import Icon from '../../elements/Icon/inde';
 
 interface CartsProps {
     carts: CartInfo[];
@@ -14,16 +15,20 @@ interface CartsProps {
 const Carts: React.FC<CartsProps> = ({ carts, setActiveCart, removeCart, handleAddCart }) => {
   return (
     <div className='carts__container'>
-      <Button onClick={handleAddCart} text="Create New Cart" className='carts__button carts__button--new' />
-      <Title type='h3' text='Existing Carts' className='carts__title' />
+      <Title type='h3' text='Existing Bags' className='carts__title' />
       <ul className='carts__list'>
         {carts.map(cart => (
           <li key={cart.id} className='carts__item'>
-            <Button onClick={() => setActiveCart(cart.id)} text={`Cart #${cart.readableId}`} className='carts__button carts__button--cart' />
-            <Button onClick={() => removeCart(cart.id)} text="Remove" className='carts__button carts__button--remove' />
+            <Button onClick={() => setActiveCart(cart.id)} text={`Bag #${cart.readableId}`} className='carts__button carts__button--cart' />
+            <button onClick={() => removeCart(cart.id)} className='carts__button carts__button--remove'>
+              <Icon color='white' size={32} icon='shopping-bag-x'/>
+            </button>
           </li>
         ))}
       </ul>
+      <button onClick={handleAddCart} className='carts__button carts__button--new' >
+        <Icon color='white' size={32} icon='shopping-bag-plus'/>
+      </button>
     </div>
   );
 };
