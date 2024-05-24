adaaf9b4f6dafc937f2df740ebc7576ce12eda1b
Sebastian_Hernandez_Patio <sebastianhernandezpatio.2004@gmail.com>
2024-05-24 09:55:45 -0500

    Add function to know if a book is already added to my cart

--- src/app/ui/components/BookList/index.tsx
+++ src/app/ui/components/BookList/index.tsx
@@ -2,6 +2,7 @@ import { BookItem } from '../BookItem';
 import { Copy } from '../../../core/models/copy.model';
 import './style.css';
 import { Title } from '../../elements/Title';
+import { useCart } from '../../../core/hooks/useCart';
 
 interface BooksListProps {
     books: Copy[];
@@ -9,12 +10,14 @@ interface BooksListProps {
   }
   
 const BooksList: React.FC<BooksListProps> = ({ books, addToCart }) => {
+  const {activeCart} = useCart();
+  const bag = activeCart?.items;
   return (
     <div className='books__container'>
       <Title type='h1' text='Books List' className='books__title'/>
       <ul className='books__list'>
         {books.map((book) => (
-          <BookItem key={book.id} book={book} isInCart={false} addToCart={addToCart}/>
+          <BookItem key={book.id} book={book} isInCart={bag?.some(item => item.id === book.id) || false} addToCart={addToCart}/>
         ))}
       </ul>
     </div>
