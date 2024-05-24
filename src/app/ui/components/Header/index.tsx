4334f275021bc76350ec95f19071ac12c00edf75
Sebastian_Hernandez_Patio <sebastianhernandezpatio.2004@gmail.com>
2024-05-24 01:24:29 -0500

    Fix header styles, add header icon, add icons

--- src/app/ui/components/Header/index.tsx
+++ src/app/ui/components/Header/index.tsx
@@ -5,10 +5,13 @@ import Icon from '../../elements/Icon/inde';
 
 export const Header = (): ReactElement => {
   return (
-    <header className='main__header'>
-      <div className="main__logo-container">
-        <Icon color='white' size={32} icon='book-2'/>
-        <Paragraph className='logo__text' text='BookStore'/>
+    <header className='header__container'>
+      <div className="header__main">
+        <div className="header__logo-container">
+          <Icon color='white' size={32} icon='book-2'/>
+          <Paragraph className='logo__text' text='BookStore'/>
+        </div>
+        <Icon color='white' size={32} icon='shopping-cart'/>
       </div>
     </header>
   );
