3b4665332f0de36294ace30131f86a3f65af15bb
Sebastian_Hernandez_Patio <sebastianhernandezpatio.2004@gmail.com>
2024-05-24 05:15:24 -0500

    Change the background color of the aside, add style to the bag section

--- src/app/ui/components/Cart/index.tsx
+++ src/app/ui/components/Cart/index.tsx
@@ -16,17 +16,21 @@ interface CartProps {
 const Cart: React.FC<CartProps> = ({ cartId, cart, onRemoveFromCart, onIncreaseQuantity, onDecreaseQuantity }) => {
   return (
     <div className='cart__container'>
-      <Title type='h2' text={`Cart #${cartId}`} className='cart__title'/>
+      <Title type='h2' text={`Bag #${cartId}`} className='cart__title'/>
       <ul className='cart__list'>
         {cart.map((item) => (
           <li key={item.id} className='cart__book'>
             <Span text={item.title} className='cart__book-title'/>
-            <div className='cart__quantity-controls'>
-              <Button onClick={() => onIncreaseQuantity(item.id)} className={'cart__book-button cart__book-button--increase'} text={'+'} />
-              <Span text={item.quantity.toString()} className='cart__book-quantity'/>
-              <Button onClick={() => onDecreaseQuantity(item.id)} className={'cart__book-button cart__book-button--decrease'} text={'-'} />
+            <div className="cart__amount">
+              <Span text='Amount:' className='cart__amount cart__amount-title'/>
+              <Span text={item.quantity.toString()} className='cart__amount'/>
             </div>
-            <Button onClick={() => onRemoveFromCart(item.id)} className={'cart__book-button cart__book-button--remove'} text={'Remove'} />
+            <div className='cart__quantity-controls'>
+              <Button onClick={() => onIncreaseQuantity(item.id)} className={'cart__book-button cart__book-button--amount'} text={'+'} />
+              <Button onClick={() => onDecreaseQuantity(item.id)} className={'cart__book-button cart__book-button--amount'} text={'-'} />
+              <Button onClick={() => onRemoveFromCart(item.id)} className={'cart__book-button cart__book-button--remove'} text={'Remove'} />
+            </div>
+            
           </li>
         ))}
       </ul>
